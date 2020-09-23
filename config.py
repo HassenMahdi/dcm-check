814aa36133508cc27ebe3379f3ddc8021c1e58c1
wajihkat <wajih.katrou@ensi-uma.tn>
2020-09-23 14:17:37 +0200

    Fix config file for azure dev environment

--- config.py
+++ config.py
@@ -1,48 +1,49 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os
+import os   
+ 
+
+basedir = os.path.abspath(os.path.dirname(__file__))    
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config:
-    """Base class for config"""
-
-    SECRET_KEY = os.getenv("SECRET_KEY", "my_precious_secret_key")
-    DEBUG = False
-
-
-class DevelopmentConfig(Config):
-    """Dev config settings"""
-
-    DEBUG = True
-    UPLOAD_FOLDER = "/scor-data/"
+class Config:   
+    
+    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')  
+    DEBUG = False   
+    UPLOAD_FOLDER = "/scor-data/"   
     MONGO_URI = "mongodb://dcm-consmos" \
                 ":pUQRAZMYnTiYikWTxjcq7zQch27litMHCSJnHOu9XCssYxBqVRWmMpd8sSnd0G7w66dQ7GMS4UK8iAvOsoBGtw==@dcm" \
                 "-consmos.mongo.cosmos.azure.com:10255/dcm?ssl=true&replicaSet=globaldb&retrywrites=false" \
-                "&maxIdleTimeMS=120000&appName=@dcm-consmos@ "
-    CORS_HEADERS = "Content-Type"
+                "&maxIdleTimeMS=120000&appName=@dcm-consmos@"
 
 
-class TestingConfig(Config):
-    """Test config settings"""
+class DevelopmentConfig(Config):    
 
-    DEBUG = True
-    TESTING = True
-
-
-class ProductionConfig(Config):
-    """Prod config settings"""
-
-    DEBUG = False
+    DEBUG = True    
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_main.db') 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  
 
 
-config_by_name = dict(
-    dev=DevelopmentConfig,
-    test=TestingConfig,
-    prod=ProductionConfig
-)
+class TestingConfig(Config):    
+    
+    DEBUG = True    
+    TESTING = True  
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_test.db') 
+    PRESERVE_CONTEXT_ON_EXCEPTION = False   
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  
+
+
+class ProductionConfig(Config): 
+    
+    DEBUG = False   
+
+
+
+config_by_name = dict(  
+    dev=DevelopmentConfig,  
+    test=TestingConfig, 
+    prod=ProductionConfig   
+)   
 
 key = Config.SECRET_KEY
